c94ec4532f3c6ad5d24aa72e5f9659eb6c887ff7
Vitor Fraporti <vitorfraporti@hotmail.com>
2023-08-31 12:10:43 -0300

    olay olya some progress

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab1',
@@ -7,20 +8,20 @@ import { GoogleMap } from '@capacitor/google-maps';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('map')mapRef: ElementRef;
 
-  mapa?: GoogleMap;
+  mapa: GoogleMap;
 
-  constructor() {
-    setTimeout(() => {
-      this.init();
-    }, 500);
+  ionViewDidEnter(){
+    this.createMap();
   }
 
-  async init(){
+  async createMap(){
     this.mapa = await GoogleMap.create({
       id: "my_map",
-      element: document.getElementById("map")!,
-      apiKey: "AIzaSyDEalAKzT7YOti3UKWwaCsDqbkSRcj8Hsc",
+      apiKey: environment.mapsKey,
+      element: this.mapRef!.nativeElement,
+      forceCreate: true,
       config: {
         center: {
           lat: -29.720026670824154,
@@ -29,11 +30,59 @@ export class Tab1Page {
         zoom: 16
       }
     })
+  }
+}
+
+
+const northeast = [-29.710317267445962, -53.708645705427465]
+const southwest = [-29.727996585731866, -53.72328774178500]
+const teste = () =>{
+    /*this.mapa = await GoogleMap.create({
+      id: "my_map",
+      element: document.getElementById("map")!,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: -29.720026670824154,
+          lng: -53.7175518200379
+        },
+        zoom: 16
+      }
+    });*/
     let map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
       center: { lat: -29.720026670824154, lng: -53.7175518200379 },
       zoom: 16,
     });
-  }
+    let polygon = new google.maps.Polygon({
+      paths:[
+        new google.maps.LatLng(-29.71591558004931,-53.71481397158000),
+        new google.maps.LatLng(-29.71598066995641,-53.71520914011945),
+        new google.maps.LatLng(-29.71543628034292,-53.71532837200635),
+        new google.maps.LatLng(-29.71537414873194,-53.71492979684156),
+        new google.maps.LatLng(-29.71591558004931,-53.71481397158000)
+      ],
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+    });
+    let marker = new google.maps.Marker({
+      position: new google.maps.LatLng(-29.715674957530123, -53.715002212004485),
+      map: map,
+      visible: false
+    });
+    let infoWindow = new google.maps.InfoWindow({
+      content: "Area 1",
+      ariaLabel: "Area 1"
+    });
+
+    polygon.addListener("click", () => {
+      infoWindow.open({
+        anchor: marker,
+        map
+      });
+    })
+    marker.setMap(map);
+    polygon.setMap(map);
 }
-const northeast = [-29.710317267445962, -53.708645705427465]
-const southwest = [-29.727996585731866, -53.72328774178500]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapsKey: "AIzaSyDEalAKzT7YOti3UKWwaCsDqbkSRcj8Hsc"
 };
 
 /*
